411183d97ac5585531edff73db465f3b74b94325
Tiffany Brown <71472201+rosewithgoldie@users.noreply.github.com>
2021-03-09 09:34:43 -0600

    Add files via upload

---
+++ AddingNumbers.cpp
@@ -0,0 +1,22 @@
+//#include <iostream>
+//#include <string>
+//
+//int main()
+//
+//{
+//	std::cout << "Add two numbers together." << std::endl;
+//	std::cout << "Enter first number: " << std::endl;
+//	int firstNum;
+//	std::cin >> firstNum;
+//	std::cout << "Enter second number: " << std::endl;
+//	int secondNum;
+//	std::cin >> secondNum;
+//	std::cout << "The sum is: ";
+//	int sum { firstNum + secondNum };
+//	std::cout << "The sum is: " << sum << std::endl;
+//
+//	system("PAUSE");
+//
+//	return 0;
+//	
+//}
---
+++ Power Play Goal 2.cpp
@@ -0,0 +1,20 @@
+//#include <iostream>
+//#include <string>
+//
+//int main()
+//{
+//	std::cout << "What is your first name? ";
+//	std::string firstName;
+//	std::cin >> firstName;
+//	std::cout << "What is your last name? ";
+//	std::string lastName;
+//	std::cin >> lastName;
+//	std::cout << "Greetings! " << firstName << lastName << std::endl;
+//
+//	system("PAUSE");
+//
+//	return 0;
+//	
+//	
+//
+//}
